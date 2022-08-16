6849c5e40947fff8b2e570296965cafc82204516
LWQ <luwanqiang@luwanqiangdemacbook-pro.local>
2022-08-17 01:59:55 +0800

    【level】easy 【question number】66 【dateTime】2022-08-17T01:59:42.615984

--- src/main/java/arithmetic/easy/PlusOne_66.java
+++ src/main/java/arithmetic/easy/PlusOne_66.java
@@ -32,15 +32,61 @@ import java.util.Arrays;
  */
 public class PlusOne_66 {
 
+
+    /**
+     * 转成数字会有 int整型溢出的问题，会与结果不符
+     * @param digits
+     * @return
+     */
     public int[] plusOne(int[] digits) {
-        return null;
+        double temp = 0;
+        int current = digits.length -1;
+        for (int i = 0; i < digits.length; i++) {
+            temp += Math.pow(10, current) * digits[i];
+            --current;
+        }
+
+        temp += 1;
+        int size = (int) Math.log10(temp);
+        int[] result = new int[size + 1];
+        for (int i = 0; i < result.length; i++) {
+            double dd = temp/Math.pow(10, size);
+            result[i] = (int)(dd);
+            temp = temp%Math.pow(10, size);
+            --size;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         PlusOne_66 plusOne_66 = new PlusOne_66();
-        int[] digits = {1, 2, 3};
-
+        int[] digits = {6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5};
         int[] ints = plusOne_66.plusOne(digits);
         System.out.println(Arrays.toString(ints));
+
+        int[] digits2 = {1, 2, 3, 4};
+        int[] ints2 = plusOne_66.plusOne(digits);
+        System.out.println(Arrays.toString(ints2));
+    }
+
+    /**
+     *  尾数不是9，直接尾数 +1返回，
+     *  每进入下一次for循环，代表上一位为9，已经进位，本次循环再次判断是否为9，不为9当前位 +1返回，
+     *  若for循环结束未结束返回，说明所有的都是9，新建大1的数组，首位置为1，其余位默认为0
+     * @param digits
+     * @return
+     */
+    public int[] plusOne2(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        //跳出for循环，说明数字全部是9
+        int[] temp = new int[digits.length + 1];
+        temp[0] = 1;
+        return temp;
     }
 }
