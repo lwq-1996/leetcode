8fb1be57e216003fdef3d031c6bb621591a4630d
luwanqiang <luwanqiang>
2022-09-04 15:34:48 +0800

    【level】easy 【question number】58 【dateTime】2022-09-04T15:34:39.811660

--- src/main/java/arithmetic/easy/LengthOfLastWord_58.java
+++ src/main/java/arithmetic/easy/LengthOfLastWord_58.java
@@ -30,7 +30,8 @@ package arithmetic.easy;
 public class LengthOfLastWord_58 {
 
     public int lengthOfLastWord(String s) {
-        return 0;
+        String[] s1 = s.split(" ");
+        return s1[s1.length-1].length();
     }
 
     public static void main(String[] args) {
