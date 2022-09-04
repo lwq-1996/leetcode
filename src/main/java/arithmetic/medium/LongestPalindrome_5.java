eca52e3195f113093c0c8b663d826e48fe78c240
luwanqiang <luwanqiang>
2022-09-04 15:20:28 +0800

    【level】medium 【question number】5 【dateTime】2022-09-04T15:20:19.257376

--- src/main/java/arithmetic/medium/LongestPalindrome_5.java
+++ src/main/java/arithmetic/medium/LongestPalindrome_5.java
@@ -8,6 +8,8 @@ package arithmetic.medium;
  * @Description: leetcode_practice
  */
 
+import java.util.Stack;
+
 /**
  * 给你一个字符串 s，找到 s 中最长的回文子串。
  *
@@ -36,9 +38,6 @@ package arithmetic.medium;
 public class LongestPalindrome_5 {
 
     public String longestPalindrome(String s) {
-        /// TODO 双向指针？从左向右遍历，每一个字符没有匹配项就截取掉
-        /// TODO 取一个首节点，依次取匹配字符串 s，匹配上就加一位
-        /// TODO 转换出一个 Char[]，作为辅助是否更容易，但应该有不需要Char数组的方法
 
         return s;
     }
@@ -46,8 +45,44 @@ public class LongestPalindrome_5 {
     public static void main(String[] args) {
         LongestPalindrome_5 longestPalindrome_5 = new LongestPalindrome_5();
         String s = "babad";
-        String result = longestPalindrome_5.longestPalindrome(s);
+        String result = longestPalindrome_5.longestPalindrome2(s);
         System.out.println(result);
         System.out.println(s.indexOf("a"));
+    }
+
+    public String longestPalindrome2(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        // 保存起始位置，测试了用数组似乎能比全局变量稍快一点
+        int[] range = new int[2];
+        char[] str = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            // 把回文看成中间的部分全是同一字符，左右部分相对称
+            // 找到下一个与当前字符不同的字符
+            i = findLongest(str, i, range);
+        }
+        return s.substring(range[0], range[1] + 1);
+    }
+
+    public static int findLongest(char[] str, int low, int[] range) {
+        // 查找中间部分
+        int high = low;
+        while (high < str.length - 1 && str[high + 1] == str[low]) {
+            high++;
+        }
+        // 定位中间部分的最后一个字符
+        int ans = high;
+        // 从中间向左右扩散
+        while (low > 0 && high < str.length - 1 && str[low - 1] == str[high + 1]) {
+            low--;
+            high++;
+        }
+        // 记录最大长度
+        if (high - low > range[1] - range[0]) {
+            range[0] = low;
+            range[1] = high;
+        }
+        return ans;
     }
 }
