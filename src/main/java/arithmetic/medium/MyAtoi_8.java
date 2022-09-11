b4070438c2f782c6102982c38f7893a5831f3379
luwanqiang <luwanqiang>
2022-09-11 12:49:46 +0800

    【level】medium 【question number】8 【dateTime】2022-09-11T12:49:33.605815

---
+++ src/main/java/arithmetic/medium/MyAtoi_8.java
@@ -0,0 +1,130 @@
+package arithmetic.medium;
+
+/**
+ * @Projectname: leetcode
+ * @Filename: MyAtoi_8
+ * @Author: LWQ
+ * @Data:2022/9/4 22:38
+ * @Description: leetcode_practice
+ */
+
+/**
+ * 请你来实现一个myAtoi(string s)函数，使其能将字符串转换成一个 32 位有符号整数（类似 C/C++ 中的 atoi 函数）。
+ *
+ * 函数myAtoi(string s) 的算法如下：
+ *
+ * 读入字符串并丢弃无用的前导空格
+ * 检查下一个字符（假设还未到字符末尾）为正还是负号，读取该字符（如果有）。 确定最终结果是负数还是正数。 如果两者都不存在，则假定结果为正。
+ * 读入下一个字符，直到到达下一个非数字字符或到达输入的结尾。字符串的其余部分将被忽略。
+ * 将前面步骤读入的这些数字转换为整数（即，"123" -> 123， "0032" -> 32）。如果没有读入数字，则整数为 0 。必要时更改符号（从步骤 2 开始）。
+ * 如果整数数超过 32 位有符号整数范围 [−231, 231− 1] ，需要截断这个整数，使其保持在这个范围内。具体来说，小于 −231 的整数应该被固定为 −231 ，大于 231− 1 的整数应该被固定为 231− 1 。
+ * 返回整数作为最终结果。
+ * 注意：
+ *
+ * 本题中的空白字符只包括空格字符 ' ' 。
+ * 除前导空格或数字后的其余字符串外，请勿忽略 任何其他字符。
+ * 
+ *
+ * 示例1：
+ *
+ * 输入：s = "42"
+ * 输出：42
+ * 解释：加粗的字符串为已经读入的字符，插入符号是当前读取的字符。
+ * 第 1 步："42"（当前没有读入字符，因为没有前导空格）
+ *          ^
+ * 第 2 步："42"（当前没有读入字符，因为这里不存在 '-' 或者 '+'）
+ *          ^
+ * 第 3 步："42"（读入 "42"）
+ *            ^
+ * 解析得到整数 42 。
+ * 由于 "42" 在范围 [-231, 231 - 1] 内，最终结果为 42 。
+ * 示例2：
+ *
+ * 输入：s = "   -42"
+ * 输出：-42
+ * 解释：
+ * 第 1 步："   -42"（读入前导空格，但忽视掉）
+ *             ^
+ * 第 2 步："   -42"（读入 '-' 字符，所以结果应该是负数）
+ *              ^
+ * 第 3 步："   -42"（读入 "42"）
+ *                ^
+ * 解析得到整数 -42 。
+ * 由于 "-42" 在范围 [-231, 231 - 1] 内，最终结果为 -42 。
+ * 示例3：
+ *
+ * 输入：s = "4193 with words"
+ * 输出：4193
+ * 解释：
+ * 第 1 步："4193 with words"（当前没有读入字符，因为没有前导空格）
+ *          ^
+ * 第 2 步："4193 with words"（当前没有读入字符，因为这里不存在 '-' 或者 '+'）
+ *          ^
+ * 第 3 步："4193 with words"（读入 "4193"；由于下一个字符不是一个数字，所以读入停止）
+ *              ^
+ * 解析得到整数 4193 。
+ * 由于 "4193" 在范围 [-231, 231 - 1] 内，最终结果为 4193 。
+ * 
+ *
+ * 提示：
+ *
+ * 0 <= s.length <= 200
+ * s 由英文字母（大写和小写）、数字（0-9）、' '、'+'、'-' 和 '.' 组成
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/string-to-integer-atoi
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MyAtoi_8 {
+
+    public int myAtoi(String s) {
+        s = s.trim();
+        char[] chars = s.toCharArray();
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        MyAtoi_8 myAtoi_8 = new MyAtoi_8();
+        String s = "   -42 with words";
+        int i = myAtoi_8.myAtoi(s);
+        System.out.println(i);
+    }
+
+    public int myAtoi2(String str) {
+        char[] chars = str.toCharArray();
+        int n = chars.length;
+        int idx = 0;
+        while (idx < n && chars[idx] == ' ') {
+            // 去掉前导空格
+            idx++;
+        }
+        if (idx == n) {
+            //去掉前导空格以后到了末尾了
+            return 0;
+        }
+        boolean negative = false;
+        if (chars[idx] == '-') {
+            //遇到负号
+            negative = true;
+            idx++;
+        } else if (chars[idx] == '+') {
+            // 遇到正号
+            idx++;
+        } else if (!Character.isDigit(chars[idx])) {
+            // 其他符号
+            return 0;
+        }
+        int ans = 0;
+        while (idx < n && Character.isDigit(chars[idx])) {
+            int digit = chars[idx] - '0';
+            if (ans > (Integer.MAX_VALUE - digit) / 10) {
+                // 本来应该是 ans * 10 + digit > Integer.MAX_VALUE
+                // 但是 *10 和 + digit 都有可能越界，所有都移动到右边去就可以了。
+                return negative? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+            ans = ans * 10 + digit;
+            idx++;
+        }
+        return negative? -ans : ans;
+    }
+}
