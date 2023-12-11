7082220d5b4818bbfbc6a6b5d234faf293ca0d82
luwanqiang <luwanqiang>
2023-12-11 12:18:21 +0800

    未完成的列表

---
+++ src/main/java/tag/string/numbers_strings_convert/NumberToWords_273_TODO.java
@@ -0,0 +1,46 @@
+package tag.string.numbers_strings_convert;
+
+/**
+ * @Projectname: leetcode
+ * @Filename: NumberToWords_273
+ * @Author: LWQ
+ * @Data: 2023/12/4 23:07
+ * @Description: hard
+ */
+
+/**
+ * 将非负整数 num 转换为其对应的英文表示。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：num = 123
+ * 输出："One Hundred Twenty Three"
+ * 示例 2：
+ *
+ * 输入：num = 12345
+ * 输出："Twelve Thousand Three Hundred Forty Five"
+ * 示例 3：
+ *
+ * 输入：num = 1234567
+ * 输出："One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+ *
+ *
+ * 提示：
+ *
+ * 0 <= num <= 231 - 1
+ */
+public class NumberToWords_273_TODO {
+
+    public String numberToWords(int num) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+        NumberToWords_273_TODO target = new NumberToWords_273_TODO();
+        int num = 123;
+        String result = target.numberToWords(num);
+        System.out.println(result);
+    }
+}
