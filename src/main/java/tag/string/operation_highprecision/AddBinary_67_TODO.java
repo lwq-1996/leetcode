db16847d7ff2d09fc527bbba04be7d2ccb3df8c1
luwanqiang <luwanqiang>
2023-12-16 10:37:20 +0800

    【tag】string-operation_highprecision 【level】TODO 【question number】ALL 【dateTime】2023-12-16T10:37:14

---
+++ src/main/java/tag/string/operation_highprecision/AddBinary_67_TODO.java
@@ -0,0 +1,44 @@
+package tag.string.operation_highprecision;
+
+/**
+ * @Projectname: leetcode
+ * @Filename: AddBinary_67_TODO
+ * @Author: LWQ
+ * @Data: 2023/12/16 10:30
+ * @Description: easy
+ */
+
+/**
+ * 给你两个二进制字符串 a 和 b ，以二进制字符串的形式返回它们的和。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入:a = "11", b = "1"
+ * 输出："100"
+ * 示例 2：
+ *
+ * 输入：a = "1010", b = "1011"
+ * 输出："10101"
+ *
+ *
+ * 提示：
+ *
+ * 1 <= a.length, b.length <= 104
+ * a 和 b 仅由字符 '0' 或 '1' 组成
+ * 字符串如果不是 "0" ，就不含前导零
+ */
+public class AddBinary_67_TODO {
+
+    public String addBinary(String a, String b) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+        AddBinary_67_TODO target = new AddBinary_67_TODO();
+        String a = "11", b = "1";
+        String result = target.addBinary(a, b);
+        System.out.println(result);
+    }
+}
