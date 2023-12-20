dd21464a00e6bb49193236ed996eb50098426b8d
luwanqiang <luwanqiang@qq.com>
2023-12-20 12:26:19 +0800

    【tag】string-match 【level】easy 【question number】28 【dateTime】2023-12-20T12:24:51

---
+++ src/main/java/tag/string/match/StrStr_28.java
@@ -0,0 +1,138 @@
+package tag.string.match;
+
+/**
+ * @Projectname: leetcode
+ * @Filename: StrStr_28_TODO
+ * @Author: LWQ
+ * @Data: 2023/12/16 10:44
+ * @Description: easy
+ */
+
+/**
+ * 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）。如果 needle 不是 haystack 的一部分，则返回  -1 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：haystack = "sadbutsad", needle = "sad"
+ * 输出：0
+ * 解释："sad" 在下标 0 和 6 处匹配。
+ * 第一个匹配项的下标是 0 ，所以返回 0 。
+ * 示例 2：
+ *
+ * 输入：haystack = "leetcode", needle = "leeto"
+ * 输出：-1
+ * 解释："leeto" 没有在 "leetcode" 中出现，所以返回 -1 。
+ *
+ *
+ * 提示：
+ *
+ * 1 <= haystack.length, needle.length <= 104
+ * haystack 和 needle 仅由小写英文字符组成
+ */
+public class StrStr_28 {
+
+    /**
+     * 通过：
+     * 时间详情：0ms，击败 100.00%使用 Java 的用户
+     * 内存详情：40.65MB，击败 5.04%使用 Java 的用户
+     *
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public int strStr(String haystack, String needle) {
+        int result = 0;
+        while (haystack.length() > 0) {
+            if (haystack.startsWith(needle)) {
+                return result;
+            }
+            result++;
+            haystack = haystack.substring(1);
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        StrStr_28 target = new StrStr_28();
+        String haystack = "sadbutsad", needle = "sad";
+        int result = target.strStr(haystack, needle);
+        System.out.println(result);
+    }
+
+    /**
+     * @param haystack
+     * @param needle
+     * @return
+     * @desception：KMP算法
+     */
+    public int strStr2(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        if (m == 0) {
+            return 0;
+        }
+        int[] pi = new int[m];
+        for (int i = 1, j = 0; i < m; i++) {
+            while (j > 0 && needle.charAt(i) != needle.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (needle.charAt(i) == needle.charAt(j)) {
+                j++;
+            }
+            pi[i] = j;
+        }
+        for (int i = 0, j = 0; i < n; i++) {
+            while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == m) {
+                return i - m + 1;
+            }
+        }
+        return -1;
+    }
+
+    // KMP 算法
+    // ss: 原串(string)  pp: 匹配串(pattern)
+    public int strStr3(String ss, String pp) {
+        if (pp.isEmpty()) return 0;
+
+        // 分别读取原串和匹配串的长度
+        int n = ss.length(), m = pp.length();
+        // 原串和匹配串前面都加空格，使其下标从 1 开始
+        ss = " " + ss;
+        pp = " " + pp;
+
+        char[] s = ss.toCharArray();
+        char[] p = pp.toCharArray();
+
+        // 构建 next 数组，数组长度为匹配串的长度（next 数组是和匹配串相关的）
+        int[] next = new int[m + 1];
+        // 构造过程 i = 2，j = 0 开始，i 小于等于匹配串长度 【构造 i 从 2 开始】
+        for (int i = 2, j = 0; i <= m; i++) {
+            // 匹配不成功的话，j = next(j)
+            while (j > 0 && p[i] != p[j + 1]) j = next[j];
+            // 匹配成功的话，先让 j++
+            if (p[i] == p[j + 1]) j++;
+            // 更新 next[i]，结束本次循环，i++
+            next[i] = j;
+        }
+
+        // 匹配过程，i = 1，j = 0 开始，i 小于等于原串长度 【匹配 i 从 1 开始】
+        for (int i = 1, j = 0; i <= n; i++) {
+            // 匹配不成功 j = next(j)
+            while (j > 0 && s[i] != p[j + 1]) j = next[j];
+            // 匹配成功的话，先让 j++，结束本次循环后 i++
+            if (s[i] == p[j + 1]) j++;
+            // 整一段匹配成功，直接返回下标
+            if (j == m) return i - m;
+        }
+
+        return -1;
+    }
+}
