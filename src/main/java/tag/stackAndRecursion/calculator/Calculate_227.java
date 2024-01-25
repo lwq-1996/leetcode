088a1ea02293304834e3e3cfbea65938f043941a
luwanqiang <luwanqiang@qq.com>
2024-01-25 12:22:51 +0800

    【tag】stackAndRecursion-calculator 【level】medium 【question number】227 【dateTime】2024-01-25T12:22:41

---
+++ src/main/java/tag/stackAndRecursion/calculator/Calculate_227.java
@@ -0,0 +1,101 @@
+package tag.stackAndRecursion.calculator;
+
+/**
+ * @Projectname: leetcode
+ * @Filename: Calculate_227_TODO
+ * @Author: LWQ
+ * @Data: 2024/1/13 14:54
+ * @Description: medium
+ */
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
+ *
+ * 整数除法仅保留整数部分。
+ *
+ * 你可以假设给定的表达式总是有效的。所有中间结果将在 [-231, 231 - 1] 的范围内。
+ *
+ * 注意：不允许使用任何将字符串作为数学表达式计算的内置函数，比如 eval() 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：s = "3+2*2"
+ * 输出：7
+ * 示例 2：
+ *
+ * 输入：s = " 3/2 "
+ * 输出：1
+ * 示例 3：
+ *
+ * 输入：s = " 3+5 / 2 "
+ * 输出：5
+ *
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 3 * 105
+ * s 由整数和算符 ('+', '-', '*', '/') 组成，中间由一些空格隔开
+ * s 表示一个 有效表达式
+ * 表达式中的所有整数都是非负整数，且在范围 [0, 231 - 1] 内
+ * 题目数据保证答案是一个 32-bit 整数
+ */
+public class Calculate_227 {
+
+    /**
+     * 通过：
+     * 时间详情：20ms，击败 68.02%使用 Java 的用户
+     * 内存详情：44.97MB，击败 42.34%使用 Java 的用户
+     *
+     * @param s
+     * @return
+     * @desception：模拟、栈
+     *  先计算乘除，再计算加减
+     *  使用额外空间 -栈
+     */
+    public int calculate(String s) {
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+        char preSign = '+';
+        int num = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            if (Character.isDigit(s.charAt(i))) {
+                num = num * 10 + s.charAt(i) - '0';
+            }
+            if (!Character.isDigit(s.charAt(i)) && s.charAt(i) != ' ' || i == n - 1) {
+                switch (preSign) {
+                    case '+':
+                        stack.push(num);
+                        break;
+                    case '-':
+                        stack.push(-num);
+                        break;
+                    case '*':
+                        stack.push(stack.pop() * num);
+                        break;
+                    default:
+                        stack.push(stack.pop() / num);
+                }
+                preSign = s.charAt(i);
+                num = 0;
+            }
+        }
+        int ans = 0;
+        while (!stack.isEmpty()) {
+            ans += stack.pop();
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        Calculate_227 target = new Calculate_227();
+        String s = "3*2*2";
+        int result = target.calculate(s);
+        System.out.println(result);
+    }
+}
